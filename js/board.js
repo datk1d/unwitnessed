ee0d40b43f697c9e1e68a002be00097ae723f26b
Dominick Serrant <sig647@gmail.com>
2017-08-10 17:40:03 -0400

    implemented barebones HTML and started CSS styling__Landing has landed a bit

---
+++ js/board.js
@@ -0,0 +1,18 @@
+let fight ='br';
+
+
+
+  switch (fight) {
+    case 'bt':
+    case 'tb':
+      console.log('brood');
+    break;
+    case 'rb':
+    case 'br':
+    console.log('rake');
+    break;
+    case 'tr':
+    case 'rt':
+      console.log('trull');
+    break;
+  }
